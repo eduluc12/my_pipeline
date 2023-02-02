b5923ccc182e87e5be4f4a008874e6489d831e62
Eduardo <barron.luccioni@gmail.com>
2023-02-02 10:48:42 -0500

    base

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new cdk.pipelines.CodePipeline(this, 'Pipeline', {
+      synth: new cdk.pipelines.ShellStep('Synth', {
+        // Use a connection created using the AWS console to authenticate to GitHub
+        // Other sources are available.
+        input: cdk.pipelines.CodePipelineSource.connection('eduluc12/my_pipeline', 'master', {
+          connectionArn: 'arn:aws:codestar-connections:us-east-1:552355260239:connection/107e0f2f-ad86-437f-9ee0-0ec761f6cef2', // Created using the AWS console * });',
+        }),
+        commands: [
+          'npm ci',
+          'echo 1',
+          'npx cdk synth',
+        ],
+      }),
+    });
   }
 }
